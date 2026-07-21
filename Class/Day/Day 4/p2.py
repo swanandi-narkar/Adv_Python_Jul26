0fecdf6630a7102c12bc877d49ef1cb97a5b3ed4
Swanandi Narkar <swanandinarkar03@gmail.com>
2026-07-21 16:16:36 +0530

    Higher-order function

---
+++ Class/Day/Day 4/p2.py
@@ -0,0 +1,16 @@
+def apply_twice(func,value):
+    return func(func(value))
+
+def add_two(x):
+    return x+2
+
+print(apply_twice(add_two,10))
+
+#2. return a function
+def make_logger(prefix):
+    def logger(message):
+        print(f"[{prefix}] {message}")
+    return logger
+
+info_logger = make_logger("INFO")
+info_logger("System Started")
